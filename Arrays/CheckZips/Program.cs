08a4bdfa577e057c888f456f14a41a373dbebc44
fulsel <logan.fulse10@gmail.com>
2014-09-22 08:39:30 -0400

    Getting Started
    Completed Pg. 195 #1.

---
+++ Arrays/CheckZips/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckZips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Arrays/IntegerList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntegerList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] things = {1,2,3,4,5};
+            //int [] things = new int [5];
+
+            for (int i = 0; i < things.Length; i++)
+            {
+                Console.WriteLine("Thing #{0} has value {1}", i, things[i] );
+            }
+
+        }
+    }
+}
