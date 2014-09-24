4de37fc653278270626ab7321d33c0dfdeb28032
fulsel <logan.fulse10@gmail.com>
2014-09-24 08:40:46 -0400

    Almost Done
    Finished pg. 195: 1,2,4.5 and worked on 9

---
+++ Arrays/TeeBall/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeeBall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] grid = new int[12, 5];
+
+            Console.WriteLine("Please enter player number. ");
+            int number0 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the bases. ");
+            int base0 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter player number. ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the bases. ");
+            int base1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter player number. ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the bases. ");
+            int base2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter player number. ");
+            int number3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the bases. ");
+            int base3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter player number. ");
+            int number4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the bases. ");
+            int base4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter player number. ");
+            int number5 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the bases. ");
+            int base5 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter player number. ");
+            int number6 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the bases. ");
+            int base6 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter player number. ");
+            int number7 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the bases. ");
+            int base7 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter player number. ");
+            int number8 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the bases. ");
+            int base8 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter player number. ");
+            int number9 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the bases. ");
+            int base9 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter player number. ");
+            int number10 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the bases. ");
+            int base10 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter player number. ");
+            int number11 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the bases. ");
+            int base11 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Arrays/whippetBus/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace whippetBus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your trip distance in miles. ");
+            int miles = Convert.ToInt32(Console.ReadLine());
+
+            if (miles <= 99)
+                Console.WriteLine("The price of each ticket is $25.00.");
+            else if (miles >= 100 && miles <= 299)
+                Console.WriteLine("The pirce of each ticket is $40.00.");
+            else if (miles >= 300 && miles <= 499)
+                Console.WriteLine("The price of each ticket is $55.00");
+            else
+                Console.WriteLine("The price of each ticket is $70.00");
+        }
+    }
+}
